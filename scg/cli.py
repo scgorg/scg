88a014ad4775440d6875d6491bfa2e16403414e9
krnick <sungboss2004@gmail.com>
2021-12-30 11:44:09 +0800

    add encryption

--- scg/cli.py
+++ scg/cli.py
@@ -2,8 +2,12 @@ import getpass
 import json
 
 import click
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
 
 from scg import __version__
+from scg.config import key
 from scg.logo import logo
 
 logo()
@@ -31,7 +35,7 @@ def entry_point(ctx, csv):
 @entry_point.command()
 def init():
     print("====================Account Setting====================\n")
-
+    print(key)
     account = input("Please input your Gamil account: ")
 
     if account:
@@ -42,13 +46,43 @@ def init():
             print(
                 "Your account and password will be encrypted and stored in setting.json"
             )
-            with open("setting.json", "w") as json_file:
-                json.dump({"ACCOUNT": account, "PASSWORD": pwd}, json_file, indent=4)
+
+            cipher = AES.new(key, AES.MODE_CBC)
+            encoded_data = json.dumps({"ACCOUNT": account, "PASSWORD": pwd}).encode(
+                "utf-8"
+            )
+            print(encoded_data)
+            cipheredData = cipher.encrypt(pad(encoded_data, AES.block_size))
+            print(cipheredData)
+            with open("setting.bin", "wb") as f:
+                f.write(cipher.iv)
+                f.write(cipheredData)
+
         else:
             print("It looks like you don't enter your password")
 
     else:
         print("It looks like you don't enter your account name")
+
+
+@entry_point.command()
+def check():
+    print("====================Account Checking====================\n")
+
+    inputFile = "setting.bin"
+
+    with open(inputFile, "rb") as f:
+        iv = f.read(16)
+        cipheredData = f.read()
+
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+
+    originalData = unpad(cipher.decrypt(cipheredData), AES.block_size)
+    setting = json.loads(originalData.decode("utf-8"))
+    if setting["ACCOUNT"] and setting["PASSWORD"]:
+        print("Account is all set")
+    else:
+        print("Your account is not set up")
 
 
 if __name__ == "__main__":
---
+++ scg/main.py
@@ -0,0 +1,7 @@
+def main(csv):
+
+    pass
+
+
+if __name__ == "__main__":
+    pass
